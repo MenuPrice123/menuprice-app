6fec37351e88f0dffdb1f263afbc40077496d3e4
varshith14332 <varshith0304@gmail.com>
2026-01-25 15:21:55 +0530

    Edited the servicetypegrid and added a image for cloud kitchens

--- src/components/ServiceTypeGrid.tsx
+++ src/components/ServiceTypeGrid.tsx
@@ -14,8 +14,8 @@ export const ServiceTypeGrid = ({ onSelect }: ServiceTypeGridProps) => {
         },
         {
             id: "cloud-kitchen",
-            title: "Cloud Kitchen",
-            image: "https://images.unsplash.com/photo-1556910103-1c02745a30bf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80",
+            title: "Cloud Kitchens",
+            image: "/images/cloud_kitchens.png",
             icon: ChefHat
         },
         {
